0ebb94e4801ba867a2f31330cb0a8b500469bf47
MDimovski <mihajlo.d96@hotmail.com>
2017-11-13 20:03:35 +0100

    updated ui

--- app/src/main/java/com/mdkg/besilka/MainActivity.java
+++ app/src/main/java/com/mdkg/besilka/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mdkg.besilka;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnSolo = (Button) findViewById(R.id.btnSolo);
+         btnSolo.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent soloIntent = new Intent(MainActivity.this,Solo.class);
+                 startActivity(soloIntent);
+             }
+         });
     }
 }
---
+++ app/src/main/java/com/mdkg/besilka/Word.java
@@ -0,0 +1,11 @@
+package com.mdkg.besilka;
+
+/**
+ * Created by miki on 13.11.17.
+ */
+
+public class Word {
+
+
+
+}
